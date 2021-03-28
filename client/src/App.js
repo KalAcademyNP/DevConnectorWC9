516d072fd71c3f203fd382ce57ac7e61de90a9be
Kal <info@kalacademy.org>
2021-03-28 12:27:19 -0700

    React UI

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Footer from './components/layout/Footer';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Landing />
+        <Footer />
+      </div>
+    )
+  }
+}
+
