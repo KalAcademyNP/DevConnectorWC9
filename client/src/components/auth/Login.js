6703bff4a51f4719e9049b6f3b6a5e923f258fb8
Kal <info@kalacademy.org>
2021-04-18 12:18:24 -0700

    Complete UI code

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,88 +1,114 @@
 import React, { Component } from 'react'
-import {connect} from 'react-redux';
+import axios from "axios";
+import classnames from "classnames";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 import {loginUser} from '../../actions/authActions';
-import classnames from 'classnames';
 
 class Login extends Component {
-  constructor(){
+  constructor() {
     super();
+    //Local state
     this.state = {
-      email: '',
-      password: '',
-      errors: {}
-    }
-  }
-  
-  onChange(e){
-    this.setState({[e.target.name]:e.target.value });
-  }
-
-  onSubmit(e){
-    e.preventDefault();
-
-    const user = {
-      email: this.state.email,
-      password: this.state.password
+      email: "",
+      password: "",
+      errors: {},
     };
 
-     this.props.loginUser(user);  
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
-  componentWillReceiveProps(nextProps){
-    if(nextProps.auth.isAuthenticated){
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const user = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+
+    this.props.loginUser(user);
+  }
+  componentDidMount(){
+    if(this.props.auth.isAuthenticated){
       this.props.history.push('/dashboard');
     }
-    if (nextProps.errors){
-      this.setState({errors: nextProps.errors});
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated){
+      this.props.history.push('/dashboard');
+    }
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
     }
   }
+
   render() {
-    const {errors} = this.state;
+    const { errors } = this.state;
     return (
       <div className="login">
-    <div className="container">
-      <div className="row">
-        <div className="col-md-8 m-auto">
-          <h1 className="display-4 text-center">Log In</h1>
-          <p className="lead text-center">Sign in to your DevConnector account</p>
-          <form noValidate onSubmit={this.onSubmit.bind(this)}>
-            <div className="form-group">
-              <input type="email" className={classnames('form-control form-control-lg', {
-                'is-invalid': errors.email
-              })}  placeholder="Email Address" name="email" value = {this.state.email}
-              onChange={this.onChange.bind(this)} />
-              {errors.email && (
-                <div className="invalid-feedback">
-                  {errors.email}
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Log In</h1>
+              <p className="lead text-center">
+                Sign in to your DevConnector account
+              </p>
+              <form noValidate onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.email,
+                    })}
+                    placeholder="Email Address"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.onChange}
+                  />
+                  {errors.email && (
+                    <div className="invalid-feedback">{errors.email}</div>
+                  )}
                 </div>
-              )}
-            </div>
-            <div className="form-group">
-              <input type="password" className={classnames('form-control form-control-lg', {
-                'is-invalid': errors.password
-              })}  placeholder="Password" name="password" value = {this.state.password}
-              onChange={this.onChange.bind(this)} />
-              {errors.password && (
-                <div className="invalid-feedback">
-                  {errors.password}
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.password,
+                    })}
+                    placeholder="Password"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.onChange}
+                  />
+                  {errors.password && (
+                    <div className="invalid-feedback">{errors.password}</div>
+                  )}
                 </div>
-              )}
-            
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
             </div>
-            <input type="submit" className="btn btn-info btn-block mt-4" />
-          </form>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-    )
+    );
   }
 }
 
+
+Login.propTypes = {
+  loginUser: PropTypes.func.isRequired,
+  errors: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired
+};
 
 const mapStateToProps = (state) => ({
   errors: state.errors,
   auth: state.auth
 });
 
-export default connect(mapStateToProps, {loginUser})(Login);
+export default connect(mapStateToProps, { loginUser })(Login);
