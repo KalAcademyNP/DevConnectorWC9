ff5263fc58baafdb041f2e6cccdd3f4948e3b60b
Kal <info@kalacademy.org>
2021-04-25 11:54:31 -0700

    Prepare for deploy

--- server.js
+++ server.js
@@ -28,5 +28,12 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-const port = 8000;
+if (process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
+
+const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
